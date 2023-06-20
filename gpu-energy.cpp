c2d7f047218b7b51b448b6346eac4eddf5dfd997
Mats Sjöberg <mats.sjoberg@csc.fi>
2023-06-20 15:27:20 +0300

    First working version

--- gpu-energy.cpp
+++ gpu-energy.cpp
@@ -1,94 +1,91 @@
-#include <cstdio>
-#include <cstdlib>
+#include <cmath>
 #include <iostream>
-#include <fstream>
-#include <filesystem>
+#include <sstream>
 #include <vector>
 #include "rocm_smi/rocm_smi.h"
 
-#define MAX_GPU 8
-
-// g++ gpu-energy-amd.cpp -I/opt/rocm-5.2.3/include/ -L/opt/rocm-5.2.3/lib -o gpu-energy-amd -lrocm_smi64
+#define EPSILON 0.0001f
 
 using namespace std;
 
-int printError(rsmi_status_t ret) {
-  const char** statusString = NULL;
-  rsmi_status_t r = rsmi_status_string(ret, statusString);
-
-  if (r != RSMI_STATUS_SUCCESS || statusString == NULL)
-    cerr << "RSMI Error (code): " << ret << endl;
-  else
-    cerr << "RSMI Error: " << *statusString << endl;
-  return -1;
-}
-
-
 int main(int argc, char* argv[]) {
+  bool doDiff = false;
   rsmi_status_t ret;
-  uint32_t numDevices;
-  uint16_t devId;
+  uint32_t numDevices = 0;
 
   ret = rsmi_init(0);
-  if (ret != RSMI_STATUS_SUCCESS)
-    return printError(ret);
+  if (ret != RSMI_STATUS_SUCCESS) {
+    cerr << "ERROR: unable to initialize ROCm SMI library" << endl;
+    return -1;
+  }
 
   ret = rsmi_num_monitor_devices(&numDevices);
-  if (ret != RSMI_STATUS_SUCCESS)
-    return printError(ret);
-
-  if (numDevices < 1 || numDevices > MAX_GPU) {
-    cerr << "ERROR: detected invalid number of GPU devices: " << numDevices << endl;
+  if (ret != RSMI_STATUS_SUCCESS || numDevices < 1) {
+    cerr << "ERROR: unable to detect available ROCm devices" << endl;
     return -1;
   }
 
-  vector<float> prevValues(numDevices, -1);
-  
-  if (argc > 1) {
-    string inFn = argv[1];
-    cout << "Reading in previous energy values from " << inFn << endl;
-    ifstream infile(inFn);
+  vector<uint64_t> prevValues(numDevices, -1);
+  float prevResolution = -1.0;
 
-    unsigned int i;
-    uint64_t power;
-    float counterResolution;
-    while (infile >> i >> power >> counterResolution) {
-      if (i >= MAX_GPU) {
-        cerr << "ERROR: invalid GPU index: " << i << " in " << inFn << endl;
-        return -1;
-      }
-      
-      prevValues[i] = power*counterResolution;
-      cout << "READ: " << i << " " << power << " " << counterResolution << endl;
+  if (argc == (int)numDevices + 2) {
+    doDiff = true;
+    prevResolution = atof(argv[1]);
+  
+    for (int i=2; i<argc; ++i) {
+      prevValues[i-2] = atol(argv[i]);
     }
-    
+  } else if (argc != 1) {
+    cerr << "Usage: " << argv[0] << endl;
+    cerr << "       " << argv[0] << " <counterResolution> <energy1> ... <energy"
+         << numDevices << ">" << endl;
+    return -1;
   }
   
-  string fn = "gpu-energy";
-  if (const char* env_p = getenv("SLURM_JOB_ID"))
-    fn = fn + "-" + env_p;
-  
-  filesystem::path ff(filesystem::temp_directory_path() / fn);
-
-  ofstream fp(ff);
-  if (!fp) {
-    cerr << "ERROR: Unable to write to temporary file " << fn << endl;
-    return -1;
-  }    
+  float counterResolution = prevResolution;
+  ostringstream output;
 
   for (unsigned int i=0; i<numDevices; ++i) {
-    uint64_t power;
-    float counterResolution;
+    uint64_t energy;
+    float res;
     uint64_t timestamp;
-    ret = rsmi_dev_energy_count_get(i, &power, &counterResolution, &timestamp);
-    if (ret != RSMI_STATUS_SUCCESS)
-      return printError(ret);
-    fp << i << " " << power << " " << counterResolution << endl;
-  }
+    ret = rsmi_dev_energy_count_get(i, &energy, &res, &timestamp);
+    if (counterResolution < 0.0) {
+      counterResolution = res;
+      output << counterResolution << " ";
+    } else if (fabs(res - counterResolution) > EPSILON) {
+      cerr << "ERROR: counter resolution is not constant: " << res << " != "
+           << counterResolution << endl;
+      return -1;
+    }
+    counterResolution = res;
+    
+    if (ret != RSMI_STATUS_SUCCESS) {
+      cerr << "ERROR: unable to get GPU energy counter from ROCm library" << endl;
+      return -1;
+    }
 
-  fp.close();
+    if (doDiff) {
+      uint64_t prevEnergy = prevValues[i];
+      if (prevEnergy < 0.0) {
+        cerr << "Previous value for GPU " << i << " incorrect " << prevEnergy
+             << endl;
+        return -1;
+      }
+      energy = energy - prevEnergy;
+    }
+
+    if (doDiff) {
+      double energyWh = energy*(double)counterResolution/3600000.0;
+      cout << "GPU " << i << ": " << energyWh << " Wh" << endl;
+    } else  {
+      output << energy << " ";
+    }
+  }
+  if (!doDiff)
+    cout << output.str() << endl;
+
   ret = rsmi_shut_down();
 
-  cout << ff.c_str() << endl;
   return 0;
 }
